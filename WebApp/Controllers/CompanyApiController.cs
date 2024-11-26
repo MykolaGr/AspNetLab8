069108fba5a92ff3f7653a0435c87f9a376fb4da
mykola.grygorenko <grygorennik@gmail.com>
2024-11-26 13:15:34 +0100

    Initial commit

---
+++ WebApp/Controllers/CompanyApiController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApp.Controllers;
+[ApiController]
+[Route("/api/companies")]
+public class CompanyApiController: ControllerBase
+{
+    private MoviesDbContext _context;
+
+    public CompanyApiController(MoviesDbContext context)
+    {
+        _context = context;
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> GetFiltered(string fragment)
+    {
+        return Ok(
+            _context.ProductionCompanies
+                .Where(m => m.CompanyName != null && m.CompanyName.ToLower().Contains(fragment.ToLower())
+                    
+                )
+        );
+    }
+}
